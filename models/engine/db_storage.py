338c901e97a834198403d7b379f2e429f14ae0ec
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-11 10:36:54 -0500

    add and fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-'''
-    Define class DBStorage
-'''
+"""Class db storage
+"""
 import models
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
@@ -11,37 +10,39 @@ from models.base_model import BaseModel, Base
 from models.user import User
 from models.place import Place
 from models.state import State
-from models.city import City
-from models.amenity import Amenity
 from models.review import Review
+from models.amenity import Amenity
+from models.city import City
 
 
 class DBStorage:
-    '''
-        DBStorage class creates engine and a new session
-    '''
+    """BDStorage class create
+    """
     __engine = None
     __session = None
 
     def __init__(self):
-        '''initializing DBStorage object
-        '''
+        """Inicilalizacion DBStorage
+        """
         self.__engine = create_engine(
             'mysql+mysqldb://{}:{}@{}/{}'.format(
                 getenv('HBNB_MYSQL_USER'),
                 getenv('HBNB_MYSQL_PWD'),
                 getenv('HBNB_MYSQL_HOST'),
-                getenv('HBNB_MYSQL_DB')
+                getenv('HBNB_MYSQL_DB'),
             ), pool_pre_ping=True)
 
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """returns a dictionary
-        Returns:
-            returns the dictionary of the object
         """
+        Args:
+        cls ([type], optional): [description]. Defaults to None.
+        Returns:
+            [type]: [description]
+        """
+        print("$$$$$$", cls)
         obj = {}
         clss = [value for key, value in models.classes.items()]
         if cls:
@@ -49,34 +50,32 @@ class DBStorage:
                 cls = models.classes[cls]
             clss = [cls]
         for one_class in clss:
-            for value in self.__session.query(one_class):
+            for value in self.__session.query(one_class).all():
                 key = str(value.__class__.__name__) + "." + str(value.id)
                 obj[key] = value
         return obj
 
     def new(self, obj):
-        ''' Adds a new obj to the database'''
+        """add the object to the current database"""
         self.__session.add(obj)
 
     def save(self):
-        ''' Saves your current session'''
+        """commit all changes of the current database"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        ''' Deletes the current object'''
+        """delete from the current database session"""
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        ''' Reloads the engine'''
+        """create all tables in the database"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session_factory)
-
+        
     def close(self):
-        '''
-            Removes the session
-        '''
+        """removes session"""
         if self.__session:
             self.__session.close()
