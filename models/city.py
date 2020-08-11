3042844f5e0c2bfce15b30a2f740ba47d066e1dd
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-10 21:06:21 -0500

    adding city

--- models/city.py
+++ models/city.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class City(BaseModel):
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+
+class City(BaseModel, Base):
     """This is the class for State
     Attributes:
         state_id: The state id
         name: input name
     """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        states = relationship('State')
+    else:
+        name = ""
+        state_id= ""
