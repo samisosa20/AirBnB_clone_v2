bbade19dd39c5b7641ba13c097f8ac8fba16a6ce
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-12 12:15:55 -0500

    Update Place: (models/place.py)

--- models/city.py
+++ models/city.py
@@ -19,7 +19,7 @@ class City(BaseModel, Base):
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
         places = relationship(
-            "Place", cascade="all, delete-orphan", backref="cities")
+            "Place", cascade="delete", backref="cities")
     else:
         state_id = ""
         name = ""
--- models/user.py
+++ models/user.py
@@ -21,11 +21,11 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
+    __tablename__ = 'users'
     if storage_type == 'db':
-        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship(
-            'Place', cascade="all, delete-orphan", backref="user")
+            'Place', cascade="delete", backref="users")
