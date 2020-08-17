f52306796345793cf623ff192ffa382496bbe17b
Sammy Guttman <1449@holbertonschool.com>
2020-08-17 20:05:20 +0000

    change host

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@ from fabric.api import local, put, run, env
 from datetime import datetime
 import os
 
-env.hosts = ['1449-web-01', '1449-web-02']
+env.hosts = ['142.44.167.228', '35.229.95.227']
 env.user = 'ubuntu'
 
 
@@ -37,25 +37,28 @@ def do_deploy(archive_path):
     """
     if not os.path.exists(archive_path):
         return False
-    list_aux1 = archive_path.split(".tgz")
-    archive_wo_ext = "".join(list_aux1)
-    list_aux2 = archive_wo_ext.split("versions/")
-    archive_wo_ext_ver = "".join(list_aux2)
-    list_aux3 = archive_path.split("versions/")
-    archive_wo_ver = "".join(list_aux3)
-    put(archive_path, '/tmp/')
-    run("mkdir -p /data/web_static/releases/{}/".
-        format(archive_wo_ext_ver))
-    run("tar -zxf /tmp/{} -C /data/web_static/releases/{}/"
-        .format(archive_wo_ver, archive_wo_ext_ver))
-    run("rm -rf /tmp/{}".format(archive_wo_ver))
-    run("mv /data/web_static/releases/{}/web_static/*\
-    /data/web_static/releases/{}/".format(archive_wo_ext_ver,
-                                            archive_wo_ext_ver))
-    run("rm -rf /data/web_static/releases/{}/web_static".
-        format(archive_wo_ext_ver))
-    run("rm -rf /data/web_static/current")
-    run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-        format(archive_wo_ext_ver))
-    print("New version deployed!")
-    return True
+    try:
+        list_aux1 = archive_path.split(".tgz")
+        archive_wo_ext = "".join(list_aux1)
+        list_aux2 = archive_wo_ext.split("versions/")
+        archive_wo_ext_ver = "".join(list_aux2)
+        list_aux3 = archive_path.split("versions/")
+        archive_wo_ver = "".join(list_aux3)
+        put(archive_path, '/tmp/')
+        run("mkdir -p /data/web_static/releases/{}/".
+            format(archive_wo_ext_ver))
+        run("tar -zxf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(archive_wo_ver, archive_wo_ext_ver))
+        run("rm -rf /tmp/{}".format(archive_wo_ver))
+        run("mv /data/web_static/releases/{}/web_static/*\
+        /data/web_static/releases/{}/".format(archive_wo_ext_ver,
+                                                archive_wo_ext_ver))
+        run("rm -rf /data/web_static/releases/{}/web_static".
+            format(archive_wo_ext_ver))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+            format(archive_wo_ext_ver))
+        print("New version deployed!")
+        return True
+    except:
+        return False
