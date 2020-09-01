9bc10fe3e1dcc922d68094fcebf1c43f87623157
Sammy <1449@holbertonschool.com>
2020-09-01 14:28:26 -0500

    fix pycodestyle and fix point 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,6 +7,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
+from flask import Flask
+app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
@@ -27,9 +31,10 @@ def cisfun(text):
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text='is cool'):
-    """display “python ” followed by the value of the text variable"""
-    return 'python ' + text.replace('_', ' ')
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -50,5 +50,6 @@ def iseven(n):
     even = "even" if n % 2 == 0 else "odd"
     return render_template('6-number_odd_or_even.html', n=n, even=even)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
